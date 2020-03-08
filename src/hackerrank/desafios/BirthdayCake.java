e04b8009887ef1c3b0ad984eb274cb64f5eb9687
salvio <salvio.bfreire@gmail.com>
2020-03-08 16:52:02 -0300

    minMaxSum and birth cake candle

---
+++ src/hackerrank/desafios/BirthdayCake.java
@@ -0,0 +1,62 @@
+package hackerrank.desafios;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class BirthdayCake {
+
+	// Complete the birthdayCakeCandles function below.
+    static int birthdayCakeCandles(int[] ar) {
+    	
+    	Set<Long> cakeCandleList = new HashSet<>();
+    	
+    	for(int i=0; i< ar.length; i++) {
+    		cakeCandleList.add(new Long(ar[i]));
+    	}
+    	long maxResult = Collections.max(cakeCandleList);
+    	
+    	int count = 0;
+    	for(int i=0; i< ar.length; i++) {
+    		if(ar[i] == maxResult) {
+    			count++;
+    		}
+    	}
+    	return count;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+	
+	
+}
